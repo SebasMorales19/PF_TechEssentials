67d2107fc98be103a683eed8ed9e5b47377066ff
Bash_Mol <sebastian.moralleyv@hotmail.com>
2023-02-27 06:41:02 -0600

    Creacion de React App , con el nav bar y el espacio ara el listado

---
+++ TechEssentials/frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> TechEssentials</a>
+      </header>
+
+      <main>Lista de Productos</main>
+    </div>
+  );
+}
+
+export default App;
